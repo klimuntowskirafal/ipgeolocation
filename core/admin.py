f325e79ad650446fab66cb2f0d1da8069fabb607
Rafal Klimuntowski <klimuntowski.rafal@gmail.com>
2021-02-24 10:53:28 +0100

    ipmodel added

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from core.models import IpData
+
 
 # Register your models here.
+class IpDataAdmin(admin.ModelAdmin):
+    list_display = [
+        'ip',
+        'country_name',
+        'city',
+        'timestamp',
+    ]
+
+    list_filter = [
+        'country_name',
+        'city',
+        'timestamp',
+    ]
+
+
+admin.site.register(IpData, IpDataAdmin)
