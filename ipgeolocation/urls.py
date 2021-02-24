2b3460dacbce33f2bcda5c025b5fbc630cfb3f7c
Rafal Klimuntowski <klimuntowski.rafal@gmail.com>
2021-02-24 10:48:01 +0100

    main page and logging added

--- ipgeolocation/urls.py
+++ ipgeolocation/urls.py
@@ -14,8 +14,25 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path
+
+from core.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name="home"),
+    path(
+        r"login/",
+        auth_views.LoginView.as_view(
+            template_name="login.html", redirect_authenticated_user=True,
+        ),
+        name="login",
+    ),
+    path(
+        r"logout/",
+        auth_views.LogoutView.as_view(template_name="logout.html"),
+        name="logout",
+    ),
+
 ]
