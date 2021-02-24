60271ee64328b66610ebb99aab7fc4a0c99dfe59
Rafal Klimuntowski <klimuntowski.rafal@gmail.com>
2021-02-24 11:24:08 +0100

    added rest_framework

--- core/views.py
+++ core/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 
 # Create your views here.
 
@@ -21,3 +24,16 @@ def home(request):
         "ip": ip,
     }
     return render(request, "geolocation.html", context)
+
+
+class Api(APIView):
+
+    def get(self, request, *args, **kwargs):
+        test_data = {
+            "ip": "134.201.250.155",
+            "country_name": "United States",
+            "region_name": "California",
+            "city": "Los Angeles",
+            "zip": 90013,
+        }
+        return Response(test_data)
