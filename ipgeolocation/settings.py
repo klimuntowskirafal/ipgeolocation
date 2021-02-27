6f48406a813006c1a4d96b5506d6f9d50263bc7b
Rafal Klimuntowski <klimuntowski.rafal@gmail.com>
2021-02-27 16:46:30 +0100

    added JWT authorisation

--- ipgeolocation/settings.py
+++ ipgeolocation/settings.py
@@ -128,3 +128,10 @@ STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = "/"
 LOGIN_URL = 'login/'
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated'],
+    'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework_simplejwt.authentication.JWTAuthentication'],
+}
