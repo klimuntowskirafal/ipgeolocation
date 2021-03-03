f69559c685c217c062d228eac955333c5a0a2065
Rafal Klimuntowski <klimuntowski.rafal@gmail.com>
2021-03-03 01:54:57 +0100

    added local_settings
    for local testing

---
+++ ipgeolocation/settings_local.py
@@ -0,0 +1,9 @@
+from ipgeolocation.settings import *
+from ipgeolocation.credentials import SECRET, API_IPSTACK_KEY
+
+SECRET_KEY = 'CREATE_YOUR_OWN_SECRET'
+
+DEBUG = True
+
+# requires action
+API_IPSTACK_KEY = 'GET_YOUR_OWN_FREE_API_KEY_FROM_IPSTACK.COM_AND_PASTE_HERE'
